565d6c9605c33ab1d6a17e30d1067103fb6023f8
Jay <punchjay@gmail.com>
2015-03-06 15:11:39 -0800

    test

--- global/global.js
+++ global/global.js
@@ -1,4 +1,17 @@
 /// <reference path="jquery-2.1.3.js" />
 $(document).ready(function () {
-//    $('.section01').addClass('fade-in-r-slide');
+    //$('.section01').addClass('fade-in-r-slide');
+    $('.bt-one').click(function () {
+
+        $('html').animate({
+            scrollTop: $('.section01').offset().top
+        }, 800);
+    });
+
+    $('.bt-two').click(function () {
+
+        $('html').animate({
+            scrollTop: $('.section02').offset().top
+        }, 800);
+    });
 });
