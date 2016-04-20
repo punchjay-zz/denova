f6551703a3ace20811ee8f44b3ac43a9af6b26a1
Jay <punchjay@gmail.com>
2016-04-20 16:00:09 -0700

    jquery to vanilla js
    jquery to vanilla js

---
+++ global/vanilla-global.js
@@ -0,0 +1,83 @@
+/// <reference path="jquery-2.1.3.js" />
+document.addEventListener("DOMContentLoaded", function () {
+
+    // MIT license - requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+    (function () {
+        var lastTime = 0;
+        var vendors = ['ms', 'moz', 'webkit', 'o'];
+        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+            window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+        }
+        if (!window.requestAnimationFrame)
+            window.requestAnimationFrame = function (callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function () {
+                        callback(currTime + timeToCall);
+                    },
+                    timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+        if (!window.cancelAnimationFrame)
+            window.cancelAnimationFrame = function (id) {
+                clearTimeout(id);
+            };
+    }());
+    //end polyfill
+
+    //nav animations and scrolling
+    //var $navBt = $('.nav-bt');
+    var navBt = document.querySelectorAll('.nav-bt');
+
+    for (var i = 0, len = navBt.length; i < len; i++) {
+        navBt[i].addEventListener('click', function () {
+
+            var thisId = this.getAttribute('id'),
+                btAnchor = '#' + thisId + '-anchor';
+
+            for (var i = 0, len = navBt.length; i < len; i++) {
+                navBt[i].classList.add('selected');
+            }
+            this.classList.remove('selected');
+            console.log(btAnchor);
+
+            //            var $this = $(this),
+            //                $thisId = $this.prop('id'),
+            //                btAnchor = '#' + $thisId + '-anchor';
+            //$navBt.removeClass('selected');
+            //            $this.addClass('selected');
+            //
+            requestAnimationFrame(function () {
+                $('html, body').animate({
+                        scrollTop: $(btAnchor).offset().top
+                    },
+                    800);
+            });
+            //
+            //            $(btAnchor).find('h1').addClass('slide-in-left').end()
+            //                .find('p').addClass('slide-in-right').end()
+            //                .find('img').addClass('fade-in');
+
+            return false;
+        });
+    }
+
+
+    //google analytics
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-64444210-1', 'auto');
+    ga('send', 'pageview');
+});
